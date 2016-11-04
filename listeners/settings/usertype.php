e15879a96de857d2b536fe0bd3b4458896eb819d
Amir.h Yeganemehr <yeganemehr@yahoo.com>
2016-11-04 17:14:47 +0330

    define permissions

---
+++ listeners/settings/usertype.php
@@ -0,0 +1,20 @@
+<?php
+namespace packages\news\listeners\settings;
+use \packages\userpanel\usertype\permissions;
+class usertype{
+	public function permissions_list(){
+		$permissions = array(
+			'list',
+			'add',
+			'edit',
+			'delete',
+
+			'comments_list',
+			'comments_edit',
+			'comments_delete'
+		);
+		foreach($permissions as $permission){
+			permissions::add('news_'.$permission);
+		}
+	}
+}
